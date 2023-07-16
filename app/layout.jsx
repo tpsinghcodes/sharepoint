6416f5eb20649745da610d915b21f93d132117c5
tpsinghcodes <tpsinghcodes@gmail.com>
2023-07-16 13:15:16 +0530

    Full Share point with Google Auth

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import "@app/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title:"Share thoughts",
+  description:"Share your AI Prompts with world"
+}
+
+const RootLayout = ({children})=> {
+  return (
+    <html lang="eng">
+      <body>
+        <Provider>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
